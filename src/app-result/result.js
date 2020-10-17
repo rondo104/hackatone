3afbabcd5086817aa2ea30d5be15545bb705411a
Roman.Harmash <roman.harmash@infopulse.com>
2020-10-17 21:27:49 +0300

    add table for result

--- src/app-result/result.js
+++ src/app-result/result.js
@@ -5,5 +5,15 @@ function getData() {
         const p = document.createElement('p');
         p.appendChild(document.createTextNode(JSON.stringify(result)));
         response.appendChild(p);
+        const tableArr = [];
+        result.questions.forEach(q => {
+            if (q.answers && q.answers.length) {
+                q.answers.forEach(a => tableArr.push({question: q.question.question, from: a.from, answer: a.answer}));
+            } else {
+                tableArr.push({question: q.question.question, from: '', answer: ''})
+            }
+        });
+        document.getElementById('table').innerHTML =
+            ConvertJsonToTable(tableArr, 'jsonTable', null, 'Download');
     })
 }
